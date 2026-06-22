a5adfbec1ba78a83782d4738847ef73cdc3fbb82
Christopher Altmann <contact@christopher-altmann.com>
2026-06-22 11:18:08 +0200

    feat: enhance getAuctions function to support dynamic query parameters and error handling

--- next-bay/lib/services/auctionsService.ts
+++ next-bay/lib/services/auctionsService.ts
@@ -27,29 +27,63 @@ export type QueryAuctionsOptions = {
   limit?: number;
 };
 
+export type ApiResponse<T> = {
+  data: T;
+  meta: {
+    page: number;
+    limit: number;
+    total: number;
+    totalPages: number;
+  };
+};
+
 export async function getAuctions(
   filter: QueryAuctionsOptions,
 ): Promise<Auction[]> {
+  const params = new URLSearchParams();
+  if (filter.status) params.append("status", filter.status);
+  if (filter.minPrice) params.append("min-price", filter.minPrice.toString());
+  if (filter.maxPrice) params.append("max-price", filter.maxPrice.toString());
+  if (filter.page) params.append("page", filter.page.toString());
+  if (filter.limit) params.append("limit", filter.limit.toString());
+
   const response = await fetch(
-    `${process.env.DARKBAY_API_URL}/auctions?page=${1}&limit=${10}`,
+    `${process.env.DARKBAY_API_URL}/auctions?${new URLSearchParams(params)}`,
   );
 
-  console.log("Response: ", response);
-  return [];
+  if (!response.ok) {
+    throw new Error(`Failed to fetch auctions: ${response.statusText}`);
+  }
+
+  const {
+    data,
+    meta: { page, limit, total, totalPages },
+  } = (await response.json()) as ApiResponse<Auction[]>;
+
+  return data;
 }
 
-getAuctions({
-  status: undefined,
-  minPrice: undefined,
-  maxPrice: undefined,
-  page: undefined,
-  limit: undefined,
-});
+export async function getAuctionById(id: string): Promise<Auction> {
+  const response = await fetch(`${process.env.DARKBAY_API_URL}/auctions/${id}`);
+  const auction = (await response.json()) as Auction;
+  return auction;
+}
 
-// export async function getAuctionById(id: string): Promise<Auction> {
-//   return auction;
-// }
-
-// export async function createAuction(dto: CreateAuction): Promise<Auction> {
-//   return auction;
-// }
+export async function createAuction(
+  dto: CreateAuction,
+  jwt_token: string,
+): Promise<Auction> {
+  const response = await fetch(`${process.env.DARKBAY_API_URL}/auctions`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt_token}`,
+    },
+    body: JSON.stringify(dto),
+  });
+  if (!response.ok) {
+    throw new Error(`Failed to create auction: ${response.statusText}`);
+  }
+  const createdAuction = (await response.json()) as Auction;
+  return createdAuction;
+}
