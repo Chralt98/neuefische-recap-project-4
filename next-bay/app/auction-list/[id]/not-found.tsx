a5fa04265fb7647daf2e3cf181be246de321d6f1
Christopher Altmann <contact@christopher-altmann.com>
2026-06-22 14:36:27 +0200

    feat: add design comments to NotFound component for auction handling

--- next-bay/app/auction-list/[id]/not-found.tsx
+++ next-bay/app/auction-list/[id]/not-found.tsx
@@ -1,3 +1,9 @@
+/*
+Design question: A user can type any id into the URL. Where do you catch the case where that auction does not exist, and what does the user see?
+
+Here, we catch it in the page component (see getAuctionById in lib/services/auctionsService.ts). If the auction is not found, we return null, which causes the page to throw an error. This error is then caught by the error.tsx file, which displays a user-friendly message.
+*/
+
 export default function NotFound() {
   return (
     <div>
